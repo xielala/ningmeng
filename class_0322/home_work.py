bbe4c76728d9713594d00f489ed66dacedb54e7d
965917155@qq.com <hhxxttxs622>
2020-03-23 23:17:11 +0800

    类 属性 函数

--- class_0322/home_work.py
+++ class_0322/home_work.py
@@ -1,3 +1,25 @@
 
 # debug-->出错的时候  哪行代码出错，就debug打在哪一行
 # 超继承  用法na'hang以及什么时候用
+
+class MathMethod:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+
+    def add(self):
+        return self.a+self.b
+
+    def sub(self):
+        return "我是父类的方法",self.a-self.b
+
+class MathMethod_1(MathMethod):
+    def divide(self):
+        return self.a/self.b
+
+    def add(self):#重写
+        super(MathMethod_1,self).add()  #超继承 :想用父类的方法，又不想重新写
+        print("我是子类的方法",self.a+self.b+10)
+
+
+MathMethod_1(5,6).add()
